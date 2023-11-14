0dea0d6ed51a5157aa8ac70ddd4fdff2547252f6
roxana.orha <roxanapascaru@gmail.com>
2023-11-14 15:11:16 +0200

    final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
+import './App.css';
+import Profile from './profile/Profile';
+import user from './profile/user.json';
+import data from './statistics/data.json';
+import Statistics from './statistics/Statistics';
+import FriendList from './friendList/FriendList';
+import friends from './friendList/friends.json';
+import transactions from './transactionhistory/transactions.json';
+import TransactionHystory from './transactionhistory/TransactionHistory';
+
+function transformDataArrayToObject(dataArray) {
+  const dataObject = {};
+  dataArray.forEach(item => {
+    dataObject[item.label] = item.percentage;
+  });
+  return dataObject;
+}
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>
+        <h1>Profile</h1>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div>
+        <Statistics
+          title="upload stats"
+          stats={transformDataArrayToObject(data)}
+        />
+      </div>
+      <div>
+        <FriendList friends={friends}></FriendList>
+      </div>
+      <div>
+        <TransactionHystory items={transactions} />;
+      </div>
     </div>
   );
-};
+}
+
+export default App;
